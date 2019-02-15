fff5e4bb5609e45fcb0e4bb048dddd13c38fe010
leoncross <leon@lscross.co.uk>
2019-02-15 17:36:13 +0000

    get param from url

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-function S_GET(id){
-    var a = new RegExp(id+"=([^&#=]*)");
-    console.log(a)
-    return decodeURIComponent(a.exec(window.location.search)[1]);
-}
-
-
-
 $( document ).ready(function() {
   var a = new RegExp(id+"=([^&#=]*)");
   console.log(a)
