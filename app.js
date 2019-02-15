8221a9a49f30f494b7a69cf5fba3fcc156ad263b
leoncross <leon@lscross.co.uk>
2019-02-15 18:10:09 +0000

    get param from url

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const fetch = require('node-fetch');
+
 $( document ).ready(function() {
   Url = {
     get get(){
@@ -12,5 +14,24 @@ $( document ).ready(function() {
     }
   };
   Url.get.location
-  console.log(Url.get.location)
+  location = Url.get.location
+
+  var self = this
+  fetch('http://api.openweathermap.org/data/2.5/forecast?q=london,uk&APPID=58c78eb23a6c145f7916168937c977c6')
+  .then(data => data.json())
+  .then(data => {
+    let promise = new Promise(function(resolve, reject) {
+      setTimeout(() => resolve("done!"), 500);
+    });
+      promise.then(
+        result => returnResults(data, self),
+      )
+  })
+  .catch(err => console.log(err));
+
+  function returnResults (data, self) {
+    console.log('complete')
+    self.result = data
+  }
+
 });
