3c0fd38dd20224a7b1f6cb25d8ee0829f8c51b54
leoncross <leon@lscross.co.uk>
2019-02-15 18:51:10 +0000

    get param from url

--- app.js
+++ app.js
@@ -16,22 +16,22 @@ $( document ).ready(function() {
   Url.get.location
   console.log(Url.get.location)
 
-  fetch('http://api.openweathermap.org/data/2.5/forecast?q=london,uk&APPID=58c78eb23a6c145f7916168937c977c6')
-  .then(data => data.json())
-  .then(data => {
-    let promise = new Promise(function(resolve, reject) {
-      setTimeout(() => resolve("done!"), 500);
-    });
-      promise.then(
-        result => returnResults(data, self),
-      )
-  })
-  .catch(err => console.log(err));
-
-  function returnResults (data, self) {
-    console.log('complete')
-    var result = data
-    console.log(result)
-  }
+  // fetch('http://api.openweathermap.org/data/2.5/forecast?q=london,uk&APPID=58c78eb23a6c145f7916168937c977c6')
+  // .then(data => data.json())
+  // .then(data => {
+  //   let promise = new Promise(function(resolve, reject) {
+  //     setTimeout(() => resolve("done!"), 500);
+  //   });
+  //     promise.then(
+  //       result => returnResults(data, self),
+  //     )
+  // })
+  // .catch(err => console.log(err));
+  //
+  // function returnResults (data, self) {
+  //   console.log('complete')
+  //   var result = data
+  //   console.log(result)
+  // }
 
 });
