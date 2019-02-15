c144eb86c8a33cbe21ae2b82ed0479a80c04bdc2
leoncross <leon@lscross.co.uk>
2019-02-15 19:03:34 +0000

    get param from url

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-var param1var = getQueryVariable("param1");
+var setLocation = getQueryVariable("location");
 
 function getQueryVariable(variable) {
   var query = window.location.search.substring(1);
-  var vars = query.split("&");
-  for (var i=0;i<vars.length;i++) {
-    var pair = vars[i].split("=");
-    if (pair[0] == variable) {
-      console.log(pair[1])
-      return pair[1];
-    }
-  }
-  alert('Query Variable ' + variable + ' not found');
+  console.log(query)
+  // var vars = query.split("&");
+  // for (var i=0;i<vars.length;i++) {
+  //   var pair = vars[i].split("=");
+  //
+  //   }
+  // }
 }
