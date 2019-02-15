89cc415ebeaf1d7007ba457660bbda56b8c71d2a
leoncross <leon@lscross.co.uk>
2019-02-15 18:01:37 +0000

    get param from url

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 $( document ).ready(function() {
-  var a = new RegExp(id+"=([^&#=]*)");
-  console.log(a)
-  return decodeURIComponent(a.exec(window.location.search)[1]);
-  console.log('hello')
+  Url = {
+    get get(){
+      var vars= {};
+      if(window.location.search.length!==0)
+        window.location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value){
+          key=decodeURIComponent(key);
+          if(typeof vars[key]==="undefined") {vars[key]= decodeURIComponent(value);}
+          else {vars[key]= [].concat(vars[key], decodeURIComponent(value));}
+        });
+      return vars;
+    }
+  };
+  Url.get.location
+  console.log(Url.get.location)
 });
--- index.php
+++ index.php
@@ -7,25 +7,7 @@
   </head>
   <body>
 
-    <script>
-      $( document ).ready(function() {
-        Url = {
-          get get(){
-            var vars= {};
-            if(window.location.search.length!==0)
-              window.location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value){
-                key=decodeURIComponent(key);
-                if(typeof vars[key]==="undefined") {vars[key]= decodeURIComponent(value);}
-                else {vars[key]= [].concat(vars[key], decodeURIComponent(value));}
-              });
-            return vars;
-          }
-        };
-        Url.get.param1
-        console.log(Url.get.param1)
-      });
-
-    </script>
+    <script src="app.js"></script>
 
 
     nothing to see here!
