6ee10ea85edcb3c8b497537662bc21f4973e5032
leoncross <leon@lscross.co.uk>
2019-02-15 17:35:56 +0000

    get param from url

--- app.js
+++ app.js
@@ -3,3 +3,13 @@ function S_GET(id){
     console.log(a)
     return decodeURIComponent(a.exec(window.location.search)[1]);
 }
+
+
+
+$( document ).ready(function() {
+  var a = new RegExp(id+"=([^&#=]*)");
+  console.log(a)
+  return decodeURIComponent(a.exec(window.location.search)[1]);
+  console.log('hello')
+
+});
