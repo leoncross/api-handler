d628a42defd5d083cba44d0dda5ed6749ae90492
leoncross <leon@lscross.co.uk>
2019-02-19 10:56:22 +0000

    resolving corbs issue

--- app.js
+++ app.js
@@ -6,14 +6,11 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.use(cors({
-  'allowedHeaders': ['sessionId', 'Content-Type'],
-  'exposedHeaders': ['sessionId'],
-  'origin': '*',
-  'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  'preflightContinue': false
-}));
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, '/public')));
