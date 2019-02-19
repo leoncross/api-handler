3a146eb7e6b41828940f95b99847b54b61c6a0da
leoncross <leon@lscross.co.uk>
2019-02-19 11:12:36 +0000

    cors error resolved; communicates with will-it-rain

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
 require('dotenv').config()
 const path = require('path');
-const cors = require('cors');
 const fetch = require('node-fetch');
 const express = require('express')
+const cors = require('./public/js/cors');
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.get('/', function(req, res){
+app.get('/', function(req, res, next){
   res.render('index', {
   });
 });
 
-app.get('/weather', function(req, res){
+app.get('/weather', function(req, res, next){
+  app.use(cors());
   location = req.query.location
   console.log(location)
   var WeatherAPI = require ('./public/js/weatherAPI.js')
